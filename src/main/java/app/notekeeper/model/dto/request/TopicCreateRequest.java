2395654f251102478c8fc5defd8240e5cde86ff5
Duong Trung Kien <kienduong2610@gmail.com>
2025-10-21 20:13:46 +0700

    fix create and update topic

--- src/main/java/app/notekeeper/model/dto/request/TopicCreateRequest.java
+++ src/main/java/app/notekeeper/model/dto/request/TopicCreateRequest.java
@@ -2,10 +2,10 @@ package app.notekeeper.model.dto.request;
 
 import lombok.Data;
 
+
 @Data
 public class TopicCreateRequest {
     private String name;
     private String description;
-    private String aiSummary;
-
 }
+
--- src/main/java/app/notekeeper/model/dto/request/TopicUpdateRequest.java
+++ src/main/java/app/notekeeper/model/dto/request/TopicUpdateRequest.java
@@ -6,5 +6,4 @@ import lombok.Data;
 public class TopicUpdateRequest {
     private String name;
     private String description;
-    private String aiSummary;
 }
